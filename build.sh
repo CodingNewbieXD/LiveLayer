ee37e3fe88daadd135eee47cbf902cfa979bfd11
CodingNewbieXD <rice.riceinwater@gmail.com>
2024-12-01 12:25:59 -0800

    remove old test file

--- build.sh
+++
@@ -1,7 +0,0 @@
-# arm
-# eval "$(/opt/homebrew/bin/brew shellenv)"
-
-# # intel
-# eval "$(/usr/local/bin/brew shellenv)"
-
-pyinstaller --optimize 2 --onefile --add-data "data:./data" --add-data "bg.gif:." --disable-windowed-traceback --add-data "icon.icns:." --windowed --icon=icon.icns background.py --noconfirm
